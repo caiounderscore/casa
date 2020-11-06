cdfbaeefb0ec87dbeb511b3745b5a9a8238c97e1
Caio Rodrigues <caiounderscore@protonmail.com>
2020-11-06 19:21:13 -0300

    update utils

--- utils/awscli.sh
+++ utils/awscli.sh
@@ -2,4 +2,3 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv
 unzip -d /tmp /tmp/awscliv2.zip
 sudo /tmp/aws/install --update
 rm -rf /tmp/awscliv2.zip /tmp/aws/
-
