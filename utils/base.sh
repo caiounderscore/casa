d6b9879f45edda3fe1a272c96223b2ddb49949db
Caio Rodrigues <caiounderscore@protonmail.com>
2020-11-09 13:05:25 -0300

    update base.sh

--- utils/base.sh
+++ utils/base.sh
@@ -1,3 +1,5 @@
 sudo apt update
 sudo apt-get install apt-transport-https curl snap vim xclip apt-transport-https ca-certificates curl gnupg-agent  software-properties-common tmux
 
+mv ~/.vimrc ~/.vimrc_old
+ln -s $PWD/.vimrc ~/.vimrc
